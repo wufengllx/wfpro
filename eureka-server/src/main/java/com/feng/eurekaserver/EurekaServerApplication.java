8977d4c77a940a9751f3b66deefcbec3171baceb
wufeng <wufeng@chetong.net>
2017-11-27 17:50:44 +0800

    spring cloud eureka 服务器搭建

---
+++ eureka-server/src/main/java/com/feng/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.feng.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author wufeng@chetong.net
+ * @date 2017/11/27 16:29
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
